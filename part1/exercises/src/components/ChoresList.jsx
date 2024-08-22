ceee5736f88454bf1692e721ad4667e85aa845cc
strickte <166240135+strickte@users.noreply.github.com>
2024-08-22 15:49:02 -0500

    React One Exercises, part1

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,26 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+import React from "react";
+import classes from "./ChoresList.module.css";
+
+class ChoresList extends React.Component {
+  render() {
+    const chores = [
+      "Sweep",
+      "Hang up laundry",
+      "Empty dishwasher",
+      "Make grocery list",
+    ];
+    return (
+      <div>
+        <h3 className={classes.choresHeading}>Today's Chores</h3>
+        <ul>
+          <li className={classes.choresText}>{chores[0]}</li>
+          <li className={classes.choresText}>{chores[1]}</li>
+          <li className={classes.choresText}>{chores[2]}</li>
+          <li className={classes.choresText}>{chores[3]}</li>
+        </ul>
+      </div>
+    );
+  }
 }
+
+export default ChoresList;
